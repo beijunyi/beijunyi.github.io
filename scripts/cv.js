25f7080db9489a6e873bdf15fa9c527c3d83104d
Junyi Bei <beijunyi@gmail.com>
2015-12-24 14:02:55 +0000

    secret reveal feature

--- scripts/cv.js
+++ scripts/cv.js
@@ -1,4 +1,17 @@
-app.controller('CvCtrl', function($scope, $route, $location) {
+app.controller('CvCtrl', function($scope, $route, $location, $uibModal) {
   var params = $location.search();
   $scope.phone = params.phone;
+
+  $scope.secretHash = 885602322;
+  $scope.revealSecret = function () {
+    var modalInstance = $uibModal.open({
+      templateUrl: 'partials/reveal-secret.html',
+      controller: function() {
+
+      },
+      size: 'sm',
+      resolve: {
+      }
+    });
+  }
 });
